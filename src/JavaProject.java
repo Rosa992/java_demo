bab481cb5ba63334f7b415cc12ed632786a3593c
Rosa <rosahamilton07@gmail.com>
2022-12-14 08:59:33 -0500

    radio button practice

--- src/JavaProject.java
+++ src/JavaProject.java
@@ -1,5 +1,29 @@
+import javax.swing.*;
+
 public class JavaProject {
     public static void main(String[] args) {
-        System.out.println("Hello, World !!!");
+
+       JFrame myJFrame = new JFrame();
+       myJFrame.setTitle("Java radio button example");
+       myJFrame.setSize(450, 300);
+
+JPanel panel = new JPanel();
+        JRadioButton Male = new JRadioButton("Male");
+        JRadioButton Female = new JRadioButton("Female");
+
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(Male);
+        bg.add(Female);
+
+        //JRadioButton is added to the panel
+        panel.add(Male);
+        panel.add(Female);
+
+        myJFrame.add(panel);
+        myJFrame.setVisible(true);
+
+
+
+
     }
 }
